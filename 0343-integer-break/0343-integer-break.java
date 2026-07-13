8272e35ae28a1d14b09821f73b2ff81101a1e843
Prateek Singh <prateek8700singh@gmail.com>
2026-07-14 01:04:16 +0530

    Time: 0 ms (100%), Space: 42.3 MB (19.77%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int integerBreak(int n) {
+        
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        int num_3 = (int)n/3;
+        int remainder = n % 3;
+        if (remainder == 1) {
+            remainder = 4;
+            num_3 --;
+        } else if (remainder == 0) 
+            remainder = 1;
+        
+        return (int)Math.pow(3, num_3) * remainder;
+        
+    }
+}
